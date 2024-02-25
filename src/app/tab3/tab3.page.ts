516ffc9ec23f11a57c8c2d21897d2c5248ee45d3
ArrysCode <arrys5777@gmail.com>
2024-02-24 21:43:25 -0500

    Implementacion niveles de agua

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
@@ -7,15 +7,35 @@ import { AngularFireDatabase } from '@angular/fire/compat/database';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  waterLevel: number = 0;
+
   constructor(public db: AngularFireDatabase) {
     this.getMeasures();
   }
 
   getMeasures() {
-    const path = "test/"
+    const path = "test/float";
+  
+    this.db.object<number | null>(path).valueChanges().subscribe((res: number | null) => {
+      if (res !== null) {
+        console.log("Medición: ", res);
+        // Convertir la distancia a porcentaje
+        this.waterLevel = this.calculateWaterLevel(res);
+      } else {
+        console.log("El valor es nulo.");
+      }
+    });
+  }
+  
 
-    this.db.list(path).valueChanges().subscribe(res => {
-      console.log("Mediciones: ", res);
-    })
+  
+
+  calculateWaterLevel(distance: number): number {
+    // Suponiendo que 1.5m representa el tanque lleno
+    const tankHeight = 150; // 1.5m en cm
+    let percentage = ((tankHeight - distance) / tankHeight) * 100;
+    if (percentage < 0) percentage = 0;
+    if (percentage > 100) percentage = 100;
+    return percentage;
   }
 }
